9b75f567501ccd79f0a291dd1fdaba9ffd6819de
Brandon Keepers <brandon@collectiveidea.com>
2009-12-01 21:37:25 -0500

    Don't import rake tasks

--- lib/passenger_debugger.rb
+++ lib/passenger_debugger.rb
@@ -1,6 +1,3 @@
-require 'rake'
-import "#{File.dirname(__FILE__)}/tasks/passenger_debugger.rake"
-
 if File.exists?(File.join(RAILS_ROOT,'tmp', 'debug.txt'))
   require 'ruby-debug'
   Debugger.wait_connection = true
